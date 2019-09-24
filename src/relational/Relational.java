4cdc02b3b6218a451059dd0415ad7221e2f45af6
fiqriakbar9 <42399322+fiqriakbar9@users.noreply.github.com>
2019-09-24 21:04:29 +0700

    Add files via upload

---
+++ src/relational/Relational.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relational;
+
+/**
+ *
+ * Nama : Fiqri Akbar Pratama
+ * NIM  : 10118020  
+ * Kelas: IF-1 
+ * Deskripsi Program : Program ini berisi program untuk menampilkan operasi 
+ * aritmatika relational 
+ * 
+ */
+
+public class Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
